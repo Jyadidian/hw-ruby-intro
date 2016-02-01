122214571a30f278a08b82e0d74e48416aa550cf
jyadidian <Joelyadidian@berkeley.edu>
2016-02-01 05:40:22 +0000

    Part 1 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,15 +4,36 @@
 
 def sum arr
   # YOUR CODE HERE
-  0
+  if arr.empty?
+    0
+  else
+    sum = 0
+    arr.each {|x| sum += x}
+    sum
+  end
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.empty?
+    0
+  elsif arr.length == 1
+    arr[0]
+  else
+    arr.sort! {|x,y| y <=> x}
+    arr[0] + arr[1] 
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if arr.empty? || arr.length == 1
+    false
+  else
+    currElem = arr.pop
+    arr.each {|x| return true if currElem + x == n}
+    return sum_to_n?(arr, n)
+  end
 end
 
 # Part 2
